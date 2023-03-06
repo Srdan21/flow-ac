8241487e97f7aa95739aa6801eaf97f1c3eecabe
Srdan21 <81490336+Srdan21@users.noreply.github.com>
2023-03-06 16:11:22 -0300

    Update godmode.lua

--- FLOW-AC-V1.0/modules/server/eventdata/godmode.lua
+++ FLOW-AC-V1.0/modules/server/eventdata/godmode.lua
@@ -10,7 +10,7 @@ function Godmode.ProcessEventData(sender, data)
     
     if DoesEntityExist(target) and IsPedAPlayer(target) then
         if GetPlayerInvincible(rawTargetData) then
-            TriggerEvent("icarus:my602oxd71pv", sender, "Godmode [C3]", false, {})
+            TriggerEvent("flow:my602oxd71pv", sender, "Godmode [C3]", false, {})
         end
     end
 end
